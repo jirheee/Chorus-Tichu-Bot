a73809bd87aeeac1fcfa6435d56d2562ee291ad9
JungInRhee <ysrheee@kaist.ac.kr>
2021-07-27 00:26:34 +0900

    test function parameters

--- src/main.py
+++ src/main.py
@@ -1,20 +1,40 @@
-from settings import DISCORD_TOKEN
+from settings import DISCORD_TOKEN, GUILD_NAME
 
-import discord
 from discord.ext import commands
+import random
+import discord
 
-bot = commands.Bot(command_prefix='Tichu.')
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print('다음으로 로그인합니다: ')
-    print(bot.user.name)
-    print('connection was succesful')
-    await bot.change_presence(status=discord.Status.online, activity=None)
+    print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'pong! {round(round(bot.latency, 4)*1000)}ms')
+# @bot.event
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send(
+#         f'Hi {member.name}, welcome to my Discord server!'
+#     )
+
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
+
+#     if message.content == "Hello":
+#         response = "Hello! I'm Chorus Tichu Bot."
+#         await message.channel.send(response)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    print("roll!")
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
 
 bot.run(DISCORD_TOKEN)
 
--- src/settings.py
+++ src/settings.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv('../.env')
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+GUILD_NAME = os.environ.get("DISCORD_GUILD")
